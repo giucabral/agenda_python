3205e7293661488a7be37ba5c5f2af7857682ea6
Giuliano Cabral <giucabral@gmail.com>
2020-05-26 07:02:45 -0300

    inserido funções de edição e exclusão de dados

--- core/views.py
+++ core/views.py
@@ -29,9 +29,6 @@ def submit_login(request):
             messages.error(request, "Usuario ou senha inválido")
     return redirect('/')
 
-
-
-
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
@@ -43,7 +40,11 @@ def lista_eventos(request):
 
 @login_required(login_url='/login/')
 def evento(request):
-    return render(request, 'evento.html')
+    id_evento = request.GET.get('id')    
+    dados = {}
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
 
 def submit_evento(request):
     if request.POST:
@@ -52,10 +53,32 @@ def submit_evento(request):
         local = request.POST.get('local')
         descricao = request.POST.get('descricao')
         usuario = request.user
-        Evento.objects.create(titulo=titulo,
-                            data_evento=data_evento,
-                            local=local,
-                            descricao=descricao,
-                            usuario=usuario)
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            evento = Evento.objects.get(id=id_evento)
+            if evento.usuario == usuario:
+                evento.titulo = titulo
+                evento.descricao = descricao
+                evento.local = local
+                evento.data_evento = data_evento
+                evento.save()
+            # Evento.objects.filter(id=id_evento).update(titulo=titulo,
+            #                                         data_evento=data_evento,
+            #                                         local=local,
+            #                                         descricao=descricao,
+            #                                         )
+        else:                                            
+            Evento.objects.create(titulo=titulo,
+                                data_evento=data_evento,
+                                local=local,
+                                descricao=descricao,
+                                usuario=usuario)
     return redirect('/')
 
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
+    return redirect('/')
